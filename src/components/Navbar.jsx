c00685a5fe31f09234b2d0a1a2ccb88840b98c40
yassine <yassineboulakhrif75@gmail.com>
2022-03-14 23:07:04 +0100

    Some CSS Added 14/03/2022

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,92 +1,3 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-
-// import cart from "../images/chariot.png";
-// import '../Styles/Nav.css';
-// import { connect } from "react-redux";
-// import { toggleNav, closeNavbar } from "./actions/itemActions";
-
-// const Navbar = (props) => {
-
-//   // const [ toggle, setToggle ] = useState(false);
-
-//   // const toggleNav = () => {
-//   //   setToggle(!toggle)
-//   // }
-
-//   return (
-//     <div className="navbar">
-//       <nav className="nav">
-//         <div className="logoBtn">
-//           <Link to="/">
-//             <p>
-//               LOGO
-//             </p>
-//           </Link>
-
-//           <div className="shopping-icon mobile-shopping">
-//             <Link to="/cart">
-//             <img src={cart} alt="cart" />
-//             <span>{props.item.cart.length}</span>
-//             </Link>
-//           </div>
-
-//           <div className="hamburger" onClick={props.toggleNav}>
-//             <div className="bar"></div>
-//             <div className="bar"></div>
-//             <div className="bar"></div>
-//           </div>
-
-//           <div className="links">
-//             <div className={props.item.cart.isOpen ? "new-links-navigation" : "links-navigation"}>
-//             {/* /* ----------------------------------------------------------- */
-//               /* <div className="nav-btn">
-//                 <span onClick={props.toggleNav}>X</span>
-//               </div> */
-//               /* ----------------------------------------------------------- */}
-//             </div>
-
-//             <ul className="links-nav" onClick={props.closeNavbar}>
-//               <li>
-//                 <Link to="/Thé">Thé</Link>
-//               </li>
-//               <li>
-//                 <Link to="/Huile">Huile</Link>
-//               </li>
-//               <li>
-//                 <Link to="/Autre">Autre</Link>
-//               </li>
-//               <li>
-//                 <Link to="/livraison">Livraison</Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-
-//         <Link to="/cart">
-//           <div className="shopping-cart">
-//             <img src={cart} alt="cart" />
-//             <span>{props.item.cart.length}</span>
-//           </div>
-//         </Link>
-//       </nav>
-//     </div> 
-//   );
-// };
-
-// const mapStateToProps = (state) => ({
-//   item:state.item
-// })
-
-// export default connect(mapStateToProps, {toggleNav, closeNavbar}) (Navbar);
-
-
-
-/* ----------------------------------------------------------------------- */
-/* ----------------------------------------------------------------------- */
-/* ----------------------------------------------------------------------- */
-
-
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
@@ -108,10 +19,10 @@ const Navbar = (props) => {
 		setOpen(false);
 	};
 
-	function GoToCart() {
-		// eslint-disable-next-line no-restricted-globals
-		history.push('/Cart')
-	}
+	// function GoToCart() {
+	// 	// eslint-disable-next-line no-restricted-globals
+	// 	history.push('/Cart')
+	// }
 
   return (
     <nav className="navbar">
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,67 +1,58 @@
-import React, {useState} from 'react'
-import BtnSlider from './BtnSlider'
-import '../Styles/Slider.css';
+import React, { useState } from "react";
+import "../Styles/Slider.css";
+
+
+
 export default function Slider() {
 
 
-
-    const dataSlider = [
-        {
-            id: 1,
-            img: "https://assets.codepen.io/1159990/tokyo-tower.jpg",
-        },
-        {
-            id: 2,
-            img: "https://images.pexels.com/photos/1816606/pexels-photo-1816606.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        },
-        {
-            id: 3,
-            img: "https://images.pexels.com/photos/3841338/pexels-photo-3841338.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        },
-        {
-            id: 4,
-            img: "https://assets.codepen.io/1159990/tokyo-tower.jpg",
-        },
-    ]
-
+  const dataSlider = [
+    {
+      id: 1,
+      img: "https://assets.codepen.io/1159990/tokyo-tower.jpg",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1816606/pexels-photo-1816606.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    },
+    {
+      id: 3,
+      img: "https://assets.codepen.io/1159990/tokyo-tower.jpg",
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/3841338/pexels-photo-3841338.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    },
+  ];
 
 
-    const [slideIndex, setSlideIndex] = useState(1)
+  const [slideIndex, setSlideIndex] = useState(1);
 
-    const nextSlide = () => {
-        if(slideIndex !== dataSlider.length){
-            setSlideIndex(slideIndex + 1)
-        } 
-        else if (slideIndex === dataSlider.length){
-            setSlideIndex(1)
-        }
-    }
+  const moveDot = index => {
+    setSlideIndex(index)
+}
 
-    const prevSlide = () => {
-        if(slideIndex !== 1){
-            setSlideIndex(slideIndex - 1)
-        }
-        else if (slideIndex === 1){
-            setSlideIndex(dataSlider.length)
-        }
-    }
+  return (
+    <div className="container-slider">
+      {dataSlider.map((obj, index) => {
+        return (
+          <div
+            key={obj.id}
+            className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+          >
+            <img src={obj.img} alt="" />
+          </div>
+        );
+      })}
 
-    return (
-        <div className="container-slider">
-
-            {dataSlider.map((obj, index) => {
-                return (
-                    <div
-                    key={obj.id}
-                    className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
-                    >
-                        <img src={obj.img} alt="" />
-                    </div>
-                )
-            })}
-            <BtnSlider moveSlide={nextSlide} direction={"next"} />
-            <BtnSlider moveSlide={prevSlide} direction={"prev"}/>
-
-        </div>
-    )
+      <div className="container-dots">
+        {Array.from({ length: 4 }).map((item, index) => (
+          <div
+            onClick={() => moveDot(index + 1)}
+            className={slideIndex === index + 1 ? "dot active" : "dot"}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
