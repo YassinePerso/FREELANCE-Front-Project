ea3a6dd84c31b7ddbce4cdc7fca2cc1c558edca6
yassine <yassineboulakhrif75@gmail.com>
2022-04-05 15:49:11 +0200

    SearchBar Done and Dead Code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,11 +19,6 @@ const Footer = () => {
       setModal(!modal);
     };
   
-    // if(modal) {
-    //   document.body.classList.add('active-modal')
-    // } else {
-    //   document.body.classList.remove('active-modal')
-    // }
 
 
     return (
